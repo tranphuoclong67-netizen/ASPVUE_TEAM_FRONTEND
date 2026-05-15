d939ce69fc58c5eec72be09ab747e746be58e07c
tranphuoclong67-netizen <tranphuoclong67@gmail.com>
2026-05-15 19:07:10 +0700

    fix base URL 2nd

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-﻿const BASE_URL = import.meta.env.PROD ? import.meta.env.VITE_API_URL : import.meta.env.VITE_API_URL
+﻿const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5035'
 
 const store = {
     token: null,
